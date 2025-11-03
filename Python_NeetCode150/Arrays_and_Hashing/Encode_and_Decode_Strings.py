08f6366aaff75f26d712d50db940621a44a6f1bd
kaiyuchen <great5361823@gmail.com>
2025-11-03 22:32:01 +0800

    modify decode function

--- Python_NeetCode150/Arrays_and_Hashing/Encode_and_Decode_Strings.py
+++ Python_NeetCode150/Arrays_and_Hashing/Encode_and_Decode_Strings.py
@@ -12,12 +12,12 @@ class Solution:
             j=i
             while s[j] != "#":  #it means s[i:j]="4" 
                 j+=1
-            length = int(s[i:j])   ## length of the next string
-        ## the actual string starts after '#'
+            length = int(s[i:j])   # length of the next string
+            # the actual string starts after '#'
             word = s[j+1:j+1+length]
             res.append(word)
             i = j + 1 +length
-        ##move index to next segment
-
+            #move index to next segment
+        return res
         #encoded = s.encode(["neet", "code", "love", "you"])
         #"4#neet4#code4#love3#you"
