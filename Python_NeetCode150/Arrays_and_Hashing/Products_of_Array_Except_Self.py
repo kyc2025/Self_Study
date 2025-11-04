aac7cdf7a18b056da20876021b4ccace5bbeb2af
kaiyuchen <great5361823@gmail.com>
2025-11-04 22:26:25 +0800

    modify O(n) solution

--- Python_NeetCode150/Arrays_and_Hashing/Products_of_Array_Except_Self.py
+++ Python_NeetCode150/Arrays_and_Hashing/Products_of_Array_Except_Self.py
@@ -10,3 +10,29 @@ class Solution:
                 product *= nums[j]
             res.append(product)
         return res
+
+#O(n)
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        res = [1] * len(nums) #initial array, ex: res = [1, 1, 1, 1]
+        #step1: left product
+        left = 1
+        for i in range(len(nums)):
+            res[i] = left
+            left *= nums[i]
+
+        #step2: right product
+        right = 1
+        for i in range(len(nums) - 1, -1, -1):      #backward: range(start, stop, step) 
+            res[i] *= right
+            right *= nums[i]
+        return res
+    
+"""
+| i  | nums[i] | prefix (left)   | res[i]         | left update            |
+| - | -------   | ------------- | ----------  | -------------------   |
+| 0 | 1           | 1                  | res[0] = 1 | prefix = 1 × 1 = 1   |
+| 1 | 2           | 1                  | res[1] = 1 | prefix = 1 × 2 = 2   |
+| 2 | 3           | 2                  | res[2] = 2 | prefix = 2 × 3 = 6   |
+| 3 | 4           | 6                  | res[3] = 6 | prefix = 6 × 4 = 24 |
+"""
